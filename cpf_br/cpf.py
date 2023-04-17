121276774bac790b0cac4f1e4c257b40c240b6ff
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-04-16 22:23:41 -0300

    feat: add comparison operation and string casting

--- cpf_br/cpf.py
+++ cpf_br/cpf.py
@@ -13,8 +13,23 @@ class CPF:
         return is_cpf_valid(cpf)
 
     def with_format(self) -> str:
-        format = "{}{}{}.{}{}{}.{}{}{}-{}{}"
-        return format.format(*list(self.__value))
+        template = "{}{}{}.{}{}{}.{}{}{}-{}{}"
+        return template.format(*list(self.__value))
 
     def value(self) -> str:
+        return self.__value
+
+    def __len__(self) -> int:
+        return len(self.__value)
+
+    def __eq__(self, cpf: object | str) -> bool:
+        if cpf is None:
+            return False
+        if type(cpf) is str:
+            return self.__value == cpf or self.with_format() == cpf
+        if not isinstance(cpf, CPF):
+            return False
+        return cpf.value() == self.__value
+
+    def __str__(self) -> str:
         return self.__value
--- cpf_br/tests/cpf_api_test.py
+++ cpf_br/tests/cpf_api_test.py
@@ -21,3 +21,51 @@ def test_should_format_cpf():
 def test_should_get_value():
     cpf = CPF("12345678909")
     assert cpf.value() == "12345678909"
+
+
+def test_should_get_length():
+    assert len(CPF("12345678909")) == 11
+
+
+def test_string_equality():
+    assert (CPF("12345678909") == "12345678909") is True
+
+
+@pytest.fixture
+def cpf_with_format_list():
+    return [
+        ["12345678909", "12345678909"],
+        ["123.456.789-09", "12345678909"],
+        ["12345678909", "123.456.789-09"],
+        ["123.456.789-09", "123.456.789-09"],
+    ]
+
+
+def test_string_equality_with_string_formatting(cpf_with_format_list: tuple[str, str]()):
+    for cpf_tuple in cpf_with_format_list:
+        [first_cpf, second_cpf] = cpf_tuple
+        assert (CPF(first_cpf) == second_cpf) is True
+
+
+def test_object_equality():
+    assert (CPF("12345678909") == CPF("12345678909")) is True
+
+
+def test_string_equality_false():
+    assert (CPF("12345678909") == "61762440059") is False
+
+
+def test_object_equality_false():
+    assert (CPF("12345678909") == CPF("61762440059")) is False
+
+
+def test_object_equality_false_with_list():
+    assert (CPF("12345678909") == ["61762440059"]) is False
+
+
+def test_none_equality():
+    assert (CPF("12345678909") == None) is False
+
+
+def test_string_casting():
+    assert str(CPF("12345678909")) is "12345678909"
